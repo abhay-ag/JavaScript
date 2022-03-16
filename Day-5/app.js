c5442e942a7ddd8019185db0bb7d0e63d2532e48
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-03-16 11:25:39 +0530

    5 practice questions

---
+++ Day-5/app.js
@@ -0,0 +1,59 @@
+// Program 8
+
+let evenNos = () =>{
+    for (let i = 2; i <= 20 ; i = i+2){
+        console.log(i)
+    }
+}
+evenNos()
+
+console.log("###################");
+
+// Program 9
+
+let oddNos = () =>{
+    for (let i = 1; i <= 20 ; i = i+2){
+        console.log(i)
+    }
+}
+oddNos()
+
+console.log("###################");
+// Program 10
+
+let sum = (n) =>{
+    let res = (n * (n+1)) / 2;
+    return res;
+}
+
+let sumRes = sum(10);
+console.log(sumRes);
+
+console.log("###################");
+
+// Program 11
+let oddSum = (n) =>{
+    let res = 0;
+    for ( let i = 1; i <= n ; i +=2){
+        res += i;
+        // console.log(i)
+    }
+    return res;
+}
+let oddSumRes = oddSum(10);
+console.log(oddSumRes);
+
+console.log("###################");
+// Program 14
+
+let avgSeries = (arr)=>{
+    let sum = 0;
+    arr.forEach((index) =>{
+        sum += index
+    })
+    return sum/arr.length;
+}
+
+let avg = avgSeries([1,2,3,4,5,6,7,8,9,10])
+
+console.log(avg);
