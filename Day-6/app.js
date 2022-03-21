05518c68e2b7159d3c4e3925f9ce4412882e6c7d
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-03-21 09:37:16 +0530

    Var Problems and The Solution Using Objects

--- Day-6/app.js
+++ Day-6/app.js
@@ -1,1 +1,33 @@
 // This Keyword
+
+/* 
+    var, array, object are all containers
+*/
+
+/*
+    var mob_make = "Apple"
+    var mob_model = "11 Pro"
+    var price = 69000
+
+    function wireless_chargin(){
+        document.write("Supports")
+    }
+
+    in this case the problem will be raised when we have to store 100 different mobiles.
+    In that situation we have to use the same properties again and again for 100 different mobiles.
+    and we can copy and paste the same piece of code repeatedly and we have to change the values of these 
+    properties every time for our particular mobile. So this is the inefficient way to tackle such kind of problems
+*/
+
+// Using Object
+
+let mob = {
+    "make" : "Apple",
+    "model": "11 Pro",
+    "price": "69000",
+    "wireless_charging":()=>{
+        document.write(mob["make"] + " " +mob["model"]+ " supports wireless charging.")
+    }
+}
+
+mob.wireless_charging()
