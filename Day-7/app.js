74d438ac3a863a29ef2f42a6cc0f32a0df563ac4
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-03-22 10:17:40 +0530

    Steps for conversion of HLL to ML

--- Day-7/app.js
+++ Day-7/app.js
@@ -1,1 +1,10 @@
 // Variable and function hoisting
+
+/*
+    HOISTING => Memory allocated for a function ::
+    Web browser is the compiler for JavaScript
+    HLL -> Assembly Language -> Machine level language
+    Steps: 
+        1. Compiler reads the source code file [Input - Processing - Output]. File is filtered of variables, and memory in allocated accordingly.
+        2. Clear the memory allocated after the code is executed.
+*/
