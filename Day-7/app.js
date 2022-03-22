3b67b4526d7a3dc5c57c75e91fc2e04fb4767cfb
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-03-22 10:20:03 +0530

    What Is Hoisting?

--- Day-7/app.js
+++ Day-7/app.js
@@ -8,3 +8,9 @@
         1. Compiler reads the source code file [Input - Processing - Output]. File is filtered of variables, and memory in allocated accordingly.
         2. Clear the memory allocated after the code is executed.
 */
+
+/* 
+    Hoisting move the variables and function declarations to the top of their scope before code is executed.
+
+    No matter where the function is declared it is hoisted to the top of the code file.
+*/
