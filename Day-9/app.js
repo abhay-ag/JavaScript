103d0d9726c0093dd12de2eb621a632cb396c47a
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-03-28 09:23:16 +0530

    About Objects

--- Day-9/app.js
+++ Day-9/app.js
@@ -1,4 +1,12 @@
 /* 
     Objects:
-        
+        1. An object is a collection of properties, and a property is an association between a key and a value
+        2. A function can be a property's value and is known as a method
+            Declaration : 
+                Eg: const func = {
+                        result : function(){
+                            let num1 = 90;
+                        }
+                    }
+        3. It's a standalone entity and it binds properties and has its type.
 */
