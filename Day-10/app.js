2e9dda3a4572c6ca8c3b4a967bd86af1a0c81eb6
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-03-30 11:04:35 +0530

    Class Definition and Properties

--- Day-10/app.js
+++ Day-10/app.js
@@ -11,3 +11,14 @@ class Car{
 
 let myCar = new Car("Ford", 2014)
 console.log("My car is " + myCar.age() + " years old.");
+
+/*
+    Class Definition:
+        1. The number of properties and methods in that class
+
+    Class can contain:
+        1. Only one constructor of each type
+            a. Empty Constructor ==> constructor() {...}
+            b. Parameterized Constructor ==> constructor(param1, param2, ...) {...}
+        2. Multiple methods
+*/
