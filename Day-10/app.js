3bb20b5e3e82af6b8d2f46dddc45bbeec4087aa7
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-03-30 11:24:31 +0530

    About instances and Static Variables

--- Day-10/app.js
+++ Day-10/app.js
@@ -21,4 +21,15 @@ console.log("My car is " + myCar.age() + " years old.");
             a. Empty Constructor ==> constructor() {...}
             b. Parameterized Constructor ==> constructor(param1, param2, ...) {...}
         2. Multiple methods
+
+    When a new object/instance is created then the instance refers to all the properties held by the original class and can access all of these.
+    Every object/instance created can not see the data of other objects.
+
+    Anything passed into the class brackets is automatically passed onto the constructor.
+*/
+/*
+    STATIC VARIABLES:
+        1. Memory not allocated again and again; have a common memory
+        2. Static can be a property and a method.
+        3. Static properties are useful for making caches and other data that is not repeated for other instances
 */
