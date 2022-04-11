1c3b4680dcbd18b25ccff49e142dd529cc1b80e7
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-04-11 09:42:22 +0530

    more about jQuery

--- Day-11/app.js
+++ Day-11/app.js
@@ -7,4 +7,8 @@
     2. It is write less, do more library of JS
     3. jQuery combines and selects similar statements into one line.
     4. simplifies AJAX calls and DOM manipulation
+    5. lightweight library ==> client side execution
+    6. provides validation controls
+
+    Operations of jQuery are event based
 */
