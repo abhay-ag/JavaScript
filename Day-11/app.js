4980b14df9bd578e49d2c126c14548404bcd3345
abhay-ag <abhay0034.be21@chitkara.edu.in>
2022-04-11 09:15:14 +0530

    What is jQuery

---
+++ Day-11/app.js
@@ -0,0 +1,10 @@
+/*
+    jQuery is the most popular in web applications.
+
+    What is jQuery?
+
+    1. It makes javaScript much easier to use on the website.
+    2. It is write less, do more library of JS
+    3. jQuery combines and selects similar statements into one line.
+    4. simplifies AJAX calls and DOM manipulation
+*/
